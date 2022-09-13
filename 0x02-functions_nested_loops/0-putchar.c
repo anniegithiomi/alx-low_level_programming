8e9f350a2337ea08b30b2e5278d3260832dc8d57
anniegithiomi <anniegithiomi@gmail.com>
2022-09-13 01:32:11 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "_putchar.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+   char *sh = "_putchar.h";
+    
+   while (*sh)
+   {
+   
+        _putchar(*sh);
+        sh++;
+
+   }
+   _putchar('\n');
+
+   return (0);
+
+}
