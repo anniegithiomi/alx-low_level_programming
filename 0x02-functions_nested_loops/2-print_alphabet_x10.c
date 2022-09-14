5e6d5ba2e4d5cebe0186224e993a7e378d0d0784
anniegithiomi <anniegithiomi@gmail.com>
2022-09-14 02:30:00 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ * print_alphabet_x10 - function that will print the alphabet 10 times
+ */
+void print_alphabet(void)
+{
+	int ch;
+	for (ch = 'a'; ch <= 'z'; ++ch)
+		_putchar (ch);
+	_putchar('\n');
+}
+
