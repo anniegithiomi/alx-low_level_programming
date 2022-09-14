bf8a5610f6d6014c9e9589c272e70e7e6de87faa
anniegithiomi <anniegithiomi@gmail.com>
2022-09-14 00:41:59 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#​include​ ​"​stdio.h​" 
+ ​#​include​ ​"​main.h" 
+  
+ ​/​**
+
+ ​ * Description: print _putchar followed on a new line 
+ ​ * Return: always 0 
+ ​ ​*/ 
+ ​void​ ​putchar​(​char​ c); 
+ ​{​char​ c = ​"​_putchar​" 
+ ​        ​_putchar​(​'​​_'​); 
+ ​        ​_putchar​(​'​p​'​); 
+ ​        ​_putchar​(​'​u​'​); 
+ ​        ​_putchar​(​'​t​'​); 
+ ​        ​_putchar​(​'​c​'​); 
+ ​        ​_putchar​(​'​h​'​); 
+ ​        ​_putchar​(​'​a​'​); 
+ ​        ​_putchar​(​'​r​'​); 
+ ​        ​_putchar​(​'​\n​'​); 
+ ​        ​return​ (​0​); 
+ ​}
